ebaefc993ab73b1532e38d50384030f15272720b
sgomeza13 <sgomeza13@eafit.edu.co>
2023-10-28 16:29:59 -0500

    Created API directory

--- TiendaDeMotos/settings.py
+++ TiendaDeMotos/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ecommerce.apps.EcommerceConfig',
     'account.apps.AccountConfig',
+    'API.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
--- TiendaDeMotos/urls.py
+++ TiendaDeMotos/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("ecommerce.urls")),
     path('account/', include("account.urls")),
+    path('api/',include("API.urls")),
 ]
 
 handler404 = ErrorView.as_view()
